491a0ec92d20189c307073fddb01e1dc26ceb30b
JDouala <simo.joeltoguem@yahoo.fr>
2018-11-06 11:44:21 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,40 @@
+// (document).ready(
+
+function()
+{
+
+	var myDiapo = $(".diaporama1").jDiaporama({
+		delay:2,
+		theme:"border",
+		useThumbs: true,
+		thumbsDir: "img/galerie/thumbs/",
+		width:512,
+		height:288,
+		transition:"sliceBarsH"
+	});
+	
+	$("#prev").click(function(){
+		myDiapo.data("jDiaporama").prev();
+	})
+	
+	$("#next").click(function(){
+		myDiapo.data("jDiaporama").next();
+	})
+	
+	$("#decreaseSlices").click(function(){
+		nbSlices = myDiapo.data("jDiaporama").getOption('nbSlices');
+		myDiapo.data("jDiaporama").changeOption("nbSlices", --nbSlices);
+	})
+	
+	$(".diaporama1").parent().parent().parent().bind("jDiaporama:pause", function(event, pause){
+		if(!pause)
+			$("#togglePause").val("Pause");
+		else
+			$("#togglePause").val("Play");
+	})
+	
+	$("#togglePause").click(function(){
+		myDiapo.data("jDiaporama").pauseSlider();
+	})
+
+};
